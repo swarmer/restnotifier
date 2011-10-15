68bfc3795b4f596afc9b4c1847ed65f3d7bcbe2a
Anton Barkowski <swarmer.pm@gmail.com>
2011-10-15 11:54:37 +0300

    Fix i18n

--- restnotifier/trayicon.cpp
+++ restnotifier/trayicon.cpp
@@ -7,6 +7,6 @@ TrayIcon::TrayIcon(QObject *parent) :
     icon = QSharedPointer<QIcon>(new QIcon(":/cloud.png"));
     menu = QSharedPointer<QMenu>(new QMenu());
     setIcon(*icon);
-    menu->addAction("Exit", this, SIGNAL(quitScheduled()));
+    menu->addAction(tr("Exit"), this, SIGNAL(quitScheduled()));
     setContextMenu(menu.data());
 }
