6b83f406f65d31febb6cf00b4d1a88d335f9a5cd
Anton Barkowski <swarmer.pm@gmail.com>
2011-10-21 22:53:31 +0300

    Set always on top hint for message dialog

--- restnotifier/restdialog.cpp
+++ restnotifier/restdialog.cpp
@@ -4,7 +4,8 @@ RestDialog::RestDialog(QString& message, QWidget *parent) :
     QDialog(parent)
 {
     Qt::WindowFlags flags = windowFlags();
-    setWindowFlags(flags & ~Qt::WindowContextHelpButtonHint);
+    setWindowFlags((flags & ~Qt::WindowContextHelpButtonHint) |
+                   Qt::WindowStaysOnTopHint);
     ui_restDialog = QSharedPointer<Ui::RestDialog>(new Ui::RestDialog());
     ui_restDialog->setupUi(this);
     ui_restDialog->messageLabel->setText(message);
