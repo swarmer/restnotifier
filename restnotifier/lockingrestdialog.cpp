a03afd81fc151a27499232b540d9160ba03fa19a
Anton Barkovsky <swarmer.pm@gmail.com>
2012-01-14 13:32:07 +0300

    unlock screen automatically after timeout

--- restnotifier/lockingrestdialog.cpp
+++ restnotifier/lockingrestdialog.cpp
@@ -1,5 +1,6 @@
 #include <QShowEvent>
 #include <QLayout>
+
 #include "lockingrestdialog.h"
 
 #if defined Q_WS_X11
@@ -12,6 +13,7 @@
 LockingRestDialog::LockingRestDialog(QWidget *parent) :
     QDialog(parent)
 {
+    // gui
     messageContent = QSharedPointer<MessageContent>(new MessageContent);
     ui = QSharedPointer<Ui::LockingRestDialog>(new Ui::LockingRestDialog);
     ui->setupUi(this);
@@ -19,7 +21,14 @@ LockingRestDialog::LockingRestDialog(QWidget *parent) :
     QVBoxLayout *vlayout = dynamic_cast<QVBoxLayout*>(layout());
     vlayout->insertWidget(0, messageContent.data());
     adjustSize();
+
     eventLoop = new QEventLoop(this);
+    timeLeft = settings.lockTime();
+    updateTimeLeft();
+
+    // setup timer
+    timer.setInterval(1000);
+    connect(&timer, SIGNAL(timeout()), SLOT(timerTick()));
 }
 
 void LockingRestDialog::executeLocking()
@@ -30,9 +39,27 @@ void LockingRestDialog::executeLocking()
     // process events so that qt shows the window
     // X keyboard locking magic doesn't work unless the window is visible
     eventLoop->processEvents();
-    // uncomment that later, when this dialog closes automatically
-    //lockScreen();
+    lockScreen();
+    timer.start();
     eventLoop->exec();
+}
+
+void LockingRestDialog::timerTick()
+{
+    timeLeft = timeLeft.addSecs(-1);
+    updateTimeLeft();
+    if ((timeLeft.minute() == 0) && (timeLeft.second() == 0))
+    {
+        accept();
+        return;
+    }
+    timer.start();
+}
+
+void LockingRestDialog::updateTimeLeft()
+{
+    QString timeText = timeLeft.toString("mm:ss");
+    ui->timeLeftLabel->setText(timeText);
 }
 
 void LockingRestDialog::dialogClosed()
--- restnotifier/lockingrestdialog.h
+++ restnotifier/lockingrestdialog.h
@@ -4,8 +4,11 @@
 #include <QEventLoop>
 #include <QDialog>
 #include <QSharedPointer>
+#include <QTime>
+#include <QTimer>
 #include <QVBoxLayout>
 
+#include "settings.h"
 #include "messagecontent.h"
 #include "ui_lockingrestdialog.h"
 
@@ -24,11 +27,17 @@ private:
     QEventLoop *eventLoop;
     QSharedPointer<MessageContent> messageContent;
     QSharedPointer<Ui::LockingRestDialog> ui;
+    QTimer timer;
+    Settings settings;
+    QTime timeLeft;
+
+    void updateTimeLeft();
 
 private slots:
     void lockScreen();
     void unlockScreen();
     void dialogClosed();
+    void timerTick();
 };
 
 #endif // LOCKINGRESTDIALOG_H
